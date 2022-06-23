6410911ce6e6b7dae6b22cddf06cdfd7458c9354
momotaun <momotaun@student.wethinkcode.co.za>
2022-06-23 16:30:08 +0200

    last push to finish at home

---
+++ app/authentication/api/views.py
@@ -0,0 +1,22 @@
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from ..models import User
+from .serializers import RegisterUserSerializer
+from rest_framework import(
+    generics,
+    status,
+    views
+    )
+
+
+class RegisterAPIView(generics.GenericAPIView):
+    
+    serializer_class = RegisterUserSerializer
+    
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_class(data=user)
+        
+        serializer.is_valid()
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_created)
